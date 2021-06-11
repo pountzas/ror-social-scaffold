292507edf84cc504800a00919b82e9d069178172
pountzas <nikos@pountzas.gr>
2021-06-11 20:23:29 +0300

    fix friend delete error

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
 
   get 'invitations/update'
   get 'invitations/create'
-  get 'invitations/destroy'
+  delete 'invitations/destroy/:id', to: 'invitations#destroy', as: 'invitations_destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
